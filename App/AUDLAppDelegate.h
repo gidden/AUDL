3d5f5095c56b4aea5d1ae621785b80de9eab3497
rfzoellner <ryanzoellner@dyn-72-33-58-63.uwnet.wisc.edu>
2014-03-13 16:16:52 -0500

    Initial Commit

---
+++ App/AUDLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AUDLAppDelegate.h
+//  App
+//
+//  Created by Ryan Zoellner on 3/13/14.
+//  Copyright (c) 2014 AUDL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AUDLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ App/AUDLViewController.m
@@ -0,0 +1,29 @@
+//
+//  AUDLViewController.m
+//  App
+//
+//  Created by Ryan Zoellner on 3/13/14.
+//  Copyright (c) 2014 AUDL. All rights reserved.
+//
+
+#import "AUDLViewController.h"
+
+@interface AUDLViewController ()
+
+@end
+
+@implementation AUDLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
