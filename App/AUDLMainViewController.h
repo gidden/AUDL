37523723250dd9b7dfa7ef86eafdff1a7c89cd94
rfzoellner <ryanzoellner@dyn-72-33-58-63.uwnet.wisc.edu>
2014-03-13 16:45:46 -0500

    Added new files for basic structure

---
+++ App/AUDLMainViewController.h
@@ -0,0 +1,15 @@
+//
+//  AUDLMainViewController.h
+//  App
+//
+//  Created by Ryan Zoellner on 3/13/14.
+//  Copyright (c) 2014 AUDL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AUDLMainViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
+@property (strong, nonatomic) NSString *roster;
+
+@end
