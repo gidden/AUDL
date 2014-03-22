a5a74fa415bc66accd504fbf64c6364ee6a9ff14
evanrypel <erypel@wisc.edu>
2014-03-21 19:40:14 -0500

    added settings page
    started a settings page. still need to link buttons to actions

---
+++ App/AUDLSettingsTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  AUDLSettingsTableViewController.h
+//  App
+//
+//  Created by Evan Rypel on 3/21/14.
+//  Copyright (c) 2014 AUDL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AUDLSettingsTableViewController : UITableViewController
+
+//break table up into arrays
+@property (nonatomic, strong) NSArray *Setting;
+//@property (nonatomic, strong) NSArray *Switch;
+
+@end
