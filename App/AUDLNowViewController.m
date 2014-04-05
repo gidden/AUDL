0791702746344bafcc58848ce422250fd721f9a8
evanrypel <erypel@wisc.edu>
2014-04-05 13:19:23 -0500

    implemented twitter page and added a table view to the now view controller in main.storyboard.  Need to get the tweets from the server still, but will load personal twitter account associated with the phone.

--- App/AUDLNowViewController.m
+++ App/AUDLNowViewController.m
@@ -8,8 +8,13 @@
 
 #import "AUDLNowViewController.h"
 #import "SWRevealViewController.h"
+#import <Accounts/Accounts.h>
+#import <Social/Social.h>
+//#import <Twitter/Twitter.h>
 
 @interface AUDLNowViewController ()
+
+@property (strong, nonatomic) NSArray *array1;
 
 @end
 
@@ -27,6 +32,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    //call timeline when application loads
+     [self getTimeLine];
     // Do any additional setup after loading the view from its nib.
     //change color of button
     _sidebarButton.tintColor = [UIColor colorWithRed:0 green:122.0/225.0 blue:1.0 alpha:1.0];
@@ -43,6 +50,85 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)getTimeLine {
+    ACAccountStore *account = [[ACAccountStore alloc] init];
+    ACAccountType *accountType = [account
+                                  accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+    
+    [account requestAccessToAccountsWithType:accountType
+                                     options:nil completion:^(BOOL granted, NSError *error)
+     {
+         if (granted == YES)
+         {
+             NSArray *arrayOfAccounts = [account
+                                         accountsWithAccountType:accountType];
+             
+             if ([arrayOfAccounts count] > 0)
+             {
+                 ACAccount *twitterAccount = [arrayOfAccounts lastObject];
+                 
+                 //TODO make a server request here
+                 NSURL *requestURL = [NSURL URLWithString:@"http://api.twitter.com/1/statuses/home_timeline.json"];
+                 
+                 NSMutableDictionary *parameters =
+                 [[NSMutableDictionary alloc] init];
+                 [parameters setObject:@"20" forKey:@"count"];
+                 [parameters setObject:@"1" forKey:@"include_entities"];
+                 
+                 SLRequest *postRequest = [SLRequest
+                                           requestForServiceType:SLServiceTypeTwitter
+                                           requestMethod:SLRequestMethodGET
+                                           URL:requestURL parameters:parameters];
+                 
+                 postRequest.account = twitterAccount;
+                 
+                 [postRequest performRequestWithHandler:
+                  ^(NSData *responseData, NSHTTPURLResponse
+                    *urlResponse, NSError *error)
+                  {
+                      self.dataSource = [NSJSONSerialization
+                                         JSONObjectWithData:responseData
+                                         options:NSJSONReadingMutableLeaves
+                                         error:&error];
+                      
+                      if (self.dataSource.count != 0) {
+                          dispatch_async(dispatch_get_main_queue(), ^{
+                              [self.nowTableView reloadData];
+                          });
+                      }
+                  }];
+             }
+         } else {
+             // Handle failure to get account access
+         }
+     }];
+}
+
+#pragma mark -
+#pragma mark UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _dataSource.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [self.nowTableView
+                             dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]
+                initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    NSDictionary *tweet = _dataSource[[indexPath row]];
+    
+    cell.textLabel.text = tweet[@"text"];
+    return cell;
 }
 
 @end
