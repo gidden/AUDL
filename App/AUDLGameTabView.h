dc7634c3be61298099d6015bb7a2fd556c8eec71
Pshriwise <shriwise@wisc.edu>
2015-02-06 16:01:18 -0600

    Created a new class for the tabbed game view.

---
+++ App/AUDLGameTabView.h
@@ -0,0 +1,16 @@
+//
+//  AUDLGameTabView.h
+//  AUDL
+//
+//  Created by Patrick on 2/6/15.
+//  Copyright (c) 2015 AUDL. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface AUDLGameTabView : UITabBarController
+
+
+
+@end
---
+++ App/AUDLGameTabView.m
@@ -0,0 +1,25 @@
+//
+//  AUDLGameTabView.m
+//  AUDL
+//
+//  Created by Patrick on 2/6/15.
+//  Copyright (c) 2015 AUDL. All rights reserved.
+//
+
+#import "AUDLGameTabView.h"
+
+
+@implementation AUDLGameTabView
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    
+}
+
+
+
+@end
+
